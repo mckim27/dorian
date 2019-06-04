0c9f013cc86188e47bf440c83b6ed11887ca2613
mckim <bluevoice27@gmail.com>
2019-06-05 02:43:00 +0900

    feat: add coverage

---
+++ coverage/__init__.py
@@ -0,0 +1,9 @@
+#! /usr/bin/python
+# -*- coding: utf-8 -*-
+
+__name__            = 'Dorian'
+__version__         = '0.9.0'
+__author__          = 'mckim',
+__author_email__    = 'bluevoice27@gmail.com'
+__description__     = 'priavte pipeline source'
+__license__         = 'Apache License 2.0'

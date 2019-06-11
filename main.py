15f1b9ef2d1f0487c4c9012c357a78617f83459f
mckim <mckim@mtcom.co.kr>
2019-06-11 15:13:37 +0900

    refactor: file 관련 func 이동에 따른 변경.
     file 관련 func 이동에 따른 변경.

--- main.py
+++ main.py
@@ -4,52 +4,15 @@
 import time
 import io
 import os
-import sys
 import stat
 import tarfile
 from kafka import KafkaConsumer
 from scrap.daum_news_scrapper import ex_scrap
 from logzero import logger as log
+from common.file_util import get_tar_stream, open_pipe
 
 SPOUT = '/pfs/out'
 # SPOUT = './pfs/out'
-
-
-def open_pipe(path_to_file, attempts=0, timeout=2, sleep_int=5):
-    if attempts < timeout :
-        flags = os.O_WRONLY  # Refer to "man 2 open".
-        mode = stat.S_IWUSR  # This is 0o400.
-        umask = 0o777 ^ mode  # Prevents always downgrading umask to 0.
-        umask_original = os.umask(umask)
-        try:
-            file = os.open(path_to_file, flags, mode)
-            # you must open the pipe as binary to prevent line-buffering problems.
-            return os.fdopen(file, "wb")
-        except OSError as oe:
-            log.error('{0} attempt of {1}; error opening file: {2}'.format(attempts + 1, timeout, oe))
-            os.umask(umask_original)
-            time.sleep(sleep_int)
-            return open_pipe(path_to_file, attempts + 1)
-        finally:
-            os.umask(umask_original)
-    return None
-
-
-def get_tar_stream(ex_spout):
-
-    if ex_spout is None:
-        log.error('error opening file: {}'.format(SPOUT))
-        exit(-2)
-
-    log.debug("Creating tarstream...")
-    try:
-        tarStream = tarfile.open(fileobj=ex_spout, mode="w|", encoding='utf-8')
-    except tarfile.TarError as te:
-        log.error('error creating tarstream: {0}'.format(te))
-        exit(-2)
-
-    return tarStream
-
 
 if __name__ == "__main__" :
     brokers = ['192.168.0.31:9092', '192.168.0.32:9092', '192.168.0.33:9092']

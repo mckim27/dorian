d974bce0260705ffe1fbe6702b5b32c97b132771
mckim <mckim@mtcom.co.kr>
2019-06-12 19:43:41 +0900

    refactor: main function. fire 로 변경.
    main function. fire 로 변경.

--- main.py
+++ main.py
@@ -2,68 +2,37 @@
 # -*- coding: utf-8 -*-
 
 import time
-import io
-import os
-import stat
-import tarfile
-from kafka import KafkaConsumer
-from scrap.daum_news_scrapper import ex_scrap
+import fire
 from logzero import logger as log
-from common.file_util import get_tar_stream, open_pipe
+from scrap.scrapper import Scrapper
+from cleanse.cleanser import Cleanser
+from common import config
 
-SPOUT = '/pfs/out'
-# SPOUT = './pfs/out'
+
+class ActionFactory:
+
+    def __init__(self, run_mode='pipeline', in_path=None, out_path='/pfs/out',
+                 broker_hosts=None, topic_name=None, group_id=None, ):
+        # set contant value
+        config.RUN_MODE = run_mode
+        config.IN_PATH = in_path
+        config.OUT_PATH = out_path
+
+        if broker_hosts is None:
+            config.BROKER_HOSTS = ['192.168.0.31:9092', '192.168.0.32:9092', '192.168.0.33:9092']
+
+        if topic_name is None:
+            config.TOPIC_NAME = 'news_raw_contents'
+
+        if group_id is None:
+            config.GROUP_ID = 'daum_news_raw_contents_consumer'
+
+        self.scrapper = Scrapper()
+        self.cleanser = Cleanser()
+
 
 if __name__ == "__main__" :
-    brokers = ['192.168.0.31:9092', '192.168.0.32:9092', '192.168.0.33:9092']
-    consumer = KafkaConsumer(
-        'news_raw_contents',
-        auto_offset_reset='latest', group_id='daum_news_raw_contents_consumer',
-        bootstrap_servers=brokers, api_version=(0, 10),
-        consumer_timeout_ms=5000, max_poll_records=10
-    )
-
     while True:
         log.info("wating ....")
         time.sleep(3)
-
-        first_flag = True
-
-        news_meta_info_list = []
-
-        tar_stream = None
-        file_pipe = None
-
-        for msg in consumer:
-            if first_flag:
-                file_pipe = open_pipe(SPOUT)
-                tar_stream = get_tar_stream(file_pipe)
-                first_flag = False
-
-            # print(msg.key.decode('utf-8'))
-            # print(msg.value.decode('utf-8'))
-
-            raw_html = msg.value.decode('utf-8')
-            name = msg.key.decode('utf-8')
-
-            contents = ex_scrap(raw_html)
-            log.debug("contents : " + contents)
-
-            tarHeader = tarfile.TarInfo()
-            tarHeader.size = len(contents.encode('utf-8'))
-            tarHeader.mode = 0o600
-            tarHeader.name = name
-            tarHeader.mtime = time.time()
-
-            try:
-                with io.BytesIO(contents.encode('utf-8')) as ff:
-                    tar_stream.addfile(tarinfo=tarHeader, fileobj=ff)
-
-            except tarfile.TarError as te:
-                log.error('error writing contents {0} to tarstream: {1}'.format(contents, name))
-                exit(-2)
-
-        if not first_flag:
-            tar_stream.close()
-            file_pipe.close()
-            log.debug('close~~~~')
+        fire.Fire(ActionFactory)

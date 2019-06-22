1e3cb5b0a0d772b768eb476c7dba60860400eaf5
mckim <bluevoice27@gmail.com>
2019-06-23 02:48:45 +0900

    fix: add dummy test code
    add dummy test code

--- test.py
+++ test.py
@@ -1,10 +1,6 @@
 #! /usr/bin/python
 # -*- coding: utf-8 -*-
 
-import sys
-from colorama import init
-init(strip=not sys.stdout.isatty()) # strip colors if stdout is redirected
-from common.info_util import print_app_info
 
 if __name__ == "__main__" :
-    print_app_info()
+    print('test not yet inplemented...')

39b04adcb6b752cb62960a8c8240f3bbb047c22f
mckim <bluevoice27@gmail.com>
2019-06-30 02:20:06 +0900

    chore: v1.0.0 으로 버전정보 변경.
    v1.0.0 으로 버전정보 변경.

--- coverage/__init__.py
+++ coverage/__init__.py
@@ -2,7 +2,7 @@
 # -*- coding: utf-8 -*-
 
 __name__            = 'Dorian'
-__version__         = '0.11.0'
+__version__         = '1.0.0'
 __author__          = 'mckim'
 __author_email__    = 'bluevoice27@gmail.com'
 __description__     = 'korean news processing pipeline app'
